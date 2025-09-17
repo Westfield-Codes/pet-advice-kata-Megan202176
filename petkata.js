56f2a2ed004469c3ad485347cd09a2fcc73cda23
Megan202176 <megan202176@student-schoolsofwestfield.org>
2025-09-17 16:10:49 +0000

    changes

--- petkata.js
+++ petkata.js
@@ -3,6 +3,7 @@
 /* Var, Prompt, Confirm, Alert, Compound Conditional */
 
 // Create pet variable, set equal to empty quotes ("")
+
 // Ask if they are allergic, store in a boolean variable with OK/Cancel
 // If they are allergic, ask if they want a cute pet, store in a boolean variable with OK/Cancel
 // If they are not allergic, ask if they want to be outdoors with their pet.  Store it in a boolean with OK/Cancel.
--- script.js
+++ script.js
@@ -3,7 +3,14 @@
 /* Var, Prompt, Confirm, Alert, Compound Conditional */
 
 // Create pet variable, set equal to empty quotes ("")
+var pet = ""
 // Ask if they are allergic, store in a boolean variable with OK/Cancel
+var allergic = confirm("Are you alergic to fur?")
+var active = confirm("Do you have an active lifestyle?")
+if (allergic == false && active==true) pet = "dog";
+else if (allergic == false && active ==false) pet = "cat";
+else pet = "fish";
+alert("Try a " + pet);
 // If they are allergic, ask if they want a cute pet, store in a boolean variable with OK/Cancel
 // If they are not allergic, ask if they want to be outdoors with their pet.  Store it in a boolean with OK/Cancel.
 // If allergic and cute, pet =  bird
